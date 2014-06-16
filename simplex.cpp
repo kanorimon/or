be06df144f69b98e297790abb6675e7b670ee297
kanorimon <kanorimon@gmail.com>
2014-06-16 23:26:19 +0900

    add get_pibot_row

--- simplex.cpp
+++ simplex.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+//xx=column ,xy=row
+int xx,xy;
+
+//simplex tableau
 double x[10][100];
 
 void print_tableau(){
@@ -28,58 +32,75 @@ bool check_optimality(){
 			minus = true;
 			break;
 		}
+		cout << "check_optimality = " << minus << " " << x[0][i] << endl;
 	}
 	return minus;
 }
 
-int get_entering_variable(){
+int get_pibot_column(){
 	
-	int pibot = 0;
+	int pibot_column = 0;
 	for(int i=1;i<xx;i++){
 		if(x[0][i] > 0){
-			pibot = i;
+			pibot_column = i;
 			break;
 		}
 	}
 	
-	cout << "pibot = " << pibot << " " << x[0][pibot] << endl;
+	cout << "pibot_column = " << pibot_column << " " << x[0][pibot_column] << endl;
 	
-	return pibot;
+	return pibot_column;
+}
+
+
+bool check_unbounded(int pibot_column){
+	bool minus = false;
+	for(int i=1;i<xy;i++){
+		if(-1*x[i][pibot_column]*x[0][pibot_column] > 0){
+			minus = true;
+			break;
+		}
+		cout << "check_unbounded = " << minus << " " << -1*x[i][pibot_column]*x[0][pibot_column] << endl;
+	}
+	return minus;
+}
+
+int get_pibot_row(int pibot_column){
+	int pibot_row=0;
+	int mini = 100000;
+	for(int i=1;i<xy;i++){
+		if(x[i][pibot_column]*-1>0 && x[i][0]/x[i][pibot_column]*-1 < mini){
+			pibot_row = i;
+			mini = x[i][0]/x[i][pibot_column]*-1;
+		}
+	}
+
+	cout << "pibot_row = " << pibot_row << " " << x[pibot_row][0] << endl;
+
+	return pibot_row;
 }
 
 int main(){
 	
-	//xx=変数の数 ,xy=目的関数の数+制約条件の数
-	int xx,xy;
+	//load
 	cin >> xx >> xy;
-	
-	//読み込み
 	for(int i=0;i<xy;i++){
 		for(int j=0;j<xx;j++){
 			cin >> x[i][j];
 		}
 	}
 	
-	//現在の辞書を画面表示
 	print_tableau();
 	
-	//最適性判定
 	if(!check_optimality) return 0;
 	
-	//ピボット列選択
-	int pibot_row = get_entering_variable();
+	int pibot_column = get_pibot_column();
 	
-	/*
-	minus = false;
-	for(int i=1;i<7;i++){
-		if(x[4-3][pibot] > 0){
-			pibot = i;
-			minus = true;
-		}
-	}
-	*/
+	if(!check_unbounded(pibot_column)) return 0;
 	
+	int pibot_row = get_pibot_row(pibot_column);
+	
+	cout << x[pibot_row][0]/x[pibot_row][pibot_column]*-1 << endl;
 	
 	return 0;
 }
-
